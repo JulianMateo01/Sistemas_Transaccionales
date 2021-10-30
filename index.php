cab5eaef70fd5bbdd476e5578bc8f305be3e061a
brafacapi <82979526+brafacapi@users.noreply.github.com>
2021-10-30 10:25:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include("conectabd.php") ?>
+<?php include("includes/header.php") ?>
+
+
+
+<?php include("Vistas/Login.php") ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php") ?>
