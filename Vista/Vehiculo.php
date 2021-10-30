a84804a078c5d25c4f23e2a33679f085a3aa3920
brafacapi <82979526+brafacapi@users.noreply.github.com>
2021-10-30 10:47:33 -0500

    Add files via upload

---
+++ Vista/Vehiculo.php
@@ -0,0 +1,41 @@
+
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Vehiculo</h3>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend col-md-6">
+							<span class="input-group-text ">Documento Usuario<i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="Cedula o pasaporte">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend col-md-6">
+							<span class="input-group-text" >Vehiculo <i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="Placas Vehiculo">
+					</div>
+					<input class="btn btn-succes" type="submit">Consultar</input>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					No registrado?</n>
+                   
+				</div>
+				<div class="d-flex justify-content-center">
+                    <a href="#">* Registro </a> 
+                    <br/> 
+					<a href="#">* Vehiculo </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
