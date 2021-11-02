942ee4918a756e103c80d8157172b2f9e7eb49b0
Bairon2002 <56143846+Bairon2002@users.noreply.github.com>
2021-11-01 22:50:13 -0500

    Add files via upload

--- Proyecto/includes/header.php
+++ Proyecto/includes/header.php
@@ -2,14 +2,43 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Uniminuto Parqueadero</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parqueadero</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav class="nav navbar-dark bg-dark">
-    <div class="container">
-        <a href="Index.php" class="navbar-brand">Inicio Parqueadero</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="http://localhost/proyectoParqueadero/index.php">UNIMINUTO PARQUEO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/proyectoParqueadero/Vista/Registro.php">Registrar Persona</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/proyectoParqueadero/Vista/RegistroVehiculo.php">Registrar Vehiculo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/proyectoParqueadero/Vista/ConsultaCliente.php">Consulta Persona</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/proyectoParqueadero/Vista/ConsultaVehiculo.php">Consulta Vehiculo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/proyectoParqueadero/Vista/Vehiculo.php">Consulta por placa</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" href="google.com" type="submit">Buscar</button>
+      </form>
     </div>
+  </div>
 </nav>
+
+
+
